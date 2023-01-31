7624466e4a2642a2487e0248dead403fe891d975
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2023-01-31 13:20:00 +0900

    변수명 변경, lecture 이미지 삭제

--- js/results.js
+++ js/results.js
@@ -5,19 +5,20 @@ console.log(mbti)
 const result = results[mbtis[mbti]]
 
 const titleEl = document.querySelector('.page-title')
-const characterEl = document.querySelector('.character')
-const boxEls = document.querySelectorAll('.box')
+const snackEl = document.querySelector('.snack')
+// const boxEls = document.querySelectorAll('.box')
+const boxEl = document.querySelector('.box')
 const jobEls = document.querySelectorAll('.job')
-const lectureEl = document.querySelector('.lecture')
-const lectureImgEl = document.querySelector('.lecture img')
+// const lectureEl = document.querySelector('.lecture')
+// const lectureImgEl = document.querySelector('.lecture img')
 
 titleEl.innerHTML = result.title
-characterEl.src = result.character
-boxEls.forEach(function (boxEl, index) {
-  boxEl.innerHTML = result.results[index]
-})
+snackEl.src = result.snack
+// boxEls.forEach(function (boxEl, index) {
+  boxEl.innerHTML = result.results
+// })
 jobEls.forEach(function (jobEl, index) {
   jobEl.innerHTML = result.jobs[index]
 })
-lectureEl.href = result.lectureUrl
-lectureImgEl.src = result.lectureImg
+// lectureEl.href = result.lectureUrl
+// lectureImgEl.src = result.lectureImg
