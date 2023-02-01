ef4fa80986aaa252293cc16229c2e3fd26a60828
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2023-02-02 04:07:25 +0900

    result 결과화면 로직 변경

--- js/results.js
+++ js/results.js
@@ -8,17 +8,29 @@ const titleEl = document.querySelector('.page-title')
 const snackEl = document.querySelector('.snack')
 // const boxEls = document.querySelectorAll('.box')
 const boxEl = document.querySelector('.box')
-const jobEls = document.querySelectorAll('.job')
+const goodEls = document.querySelectorAll('.good')
+const badEls = document.querySelectorAll('.bad')
 // const lectureEl = document.querySelector('.lecture')
 // const lectureImgEl = document.querySelector('.lecture img')
 
+// document.body.style.backgroundColor = result.bgColor
+document.body.querySelector('.bg').style.backgroundColor = result.bgColor
+if(result.bgColor !== '#ffffff' && result.bgColor !== '#ffe919') {
+  // document.querySelector('h3').style.color = '#ffffff'
+  document.querySelectorAll('h3').forEach(function (h3El) {
+    h3El.style.color = '#ffffff'
+  })
+}
 titleEl.innerHTML = result.title
 snackEl.src = result.snack
 // boxEls.forEach(function (boxEl, index) {
   boxEl.innerHTML = result.results
 // })
-jobEls.forEach(function (jobEl, index) {
-  jobEl.innerHTML = result.jobs[index]
+goodEls.forEach(function (goodEl, index) {
+  goodEl.src = '/images/' + result.goods[index] + '.png'
+})
+badEls.forEach(function (badEl, index) {
+  badEl.src = '/images/' + result.bads[index] + '.png'
 })
 // lectureEl.href = result.lectureUrl
 // lectureImgEl.src = result.lectureImg
